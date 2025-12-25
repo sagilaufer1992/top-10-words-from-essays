82b7d4986900bec1af7472672863408360691d2c
sagilaufer1992 <sagi.laufer23@gmail.com>
2025-12-25 14:54:16 +0200

    read only relevant

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {readLines} from './fileReader.mjs'
-import { extract } from '@extractus/article-extractor'
+import {readArticle} from './readArticle.mjs'
 
 const lines = await readLines('data/endg-urls');
-const content = await extract(lines[0])
-console.log(content.content);
+const content = await readArticle(lines[0])
+console.log(content);
---
+++ src/readArticle.mjs
@@ -0,0 +1,15 @@
+import { extract } from '@extractus/article-extractor'
+import { load } from "cheerio";
+
+function extractParagraphs(html) {
+    const $ = load(html);
+
+    return $("p")
+        .map((_, el) => $(el).text().trim())
+        .get()
+        .filter(text => text.length > 0);
+}
+export async function readArticle(url) {
+    const content = (await (extract(url))).content;
+    return extractParagraphs(content);
+}
